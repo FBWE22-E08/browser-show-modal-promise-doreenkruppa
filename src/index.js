3352d4180d8e9e310ce1b298ce06e15853806b95
Doreen Kruppa <doreenkruppa@web.de>
2023-01-31 14:58:25 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./css/style.css"; //importing css
+
+const modal = document.getElementById("myModal");
+
+let promiseOfModal = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("block");
+  }, 1000 * 60);
+});
+
+promiseOfModal.then((result) => {
+  modal.style.display = result;
+});
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
